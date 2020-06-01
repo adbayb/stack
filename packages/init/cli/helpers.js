60977a35950d9f4b08ea5097dcd83385d5d5d188
Ayoub Adib <adbayb@gmail.com>
2020-06-02 01:45:41 +0200

    feat(init): process package and and dependencies

---
+++ packages/init/cli/helpers.js
@@ -0,0 +1,63 @@
+const childProcess = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const fsPromises = fs.promises;
+const { PROJECT_FOLDER } = require("./constants");
+
+const isObject = (value) => {
+	return value !== null && typeof value === "object";
+};
+
+const merge = (target, ...sources) => {
+	if (sources.length === 0) {
+		return target;
+	}
+
+	const source = sources[0];
+
+	for (const key in source) {
+		const sourceValue = source[key];
+		const targetValue = target[key];
+
+		if (isObject(sourceValue) && isObject(targetValue)) {
+			target[key] = merge(targetValue, sourceValue);
+		} else {
+			target[key] = sourceValue;
+		}
+	}
+
+	sources.shift();
+
+	return merge(target, ...sources);
+};
+
+const exec = (command) => {
+	return childProcess
+		.execSync(command)
+		.toString()
+		.replace(/(\r?\n|\r)$/, "");
+};
+
+const writeFileToProject = (baseName, content, checkIfExists) => {
+	if (checkIfExists) {
+		const destFileName = path.join(PROJECT_FOLDER, baseName);
+
+		if (fs.existsSync(destFileName)) {
+			throw new Error(
+				`File ${baseName} already exists, please consider reviewing it manually`
+			);
+		}
+	}
+
+	return fsPromises.writeFile(
+		path.join(PROJECT_FOLDER, baseName),
+		content,
+		"utf-8"
+	);
+};
+
+module.exports = {
+	exec,
+	merge,
+	writeFileToProject,
+};
--- packages/init/cli/index.js
+++ packages/init/cli/index.js
@@ -1,67 +1,10 @@
 #!/usr/bin/env node
 
-const childProcess = require("child_process");
 const path = require("path");
 const fs = require("fs");
 const fsPromises = fs.promises;
-
-const PROJECT_FOLDER = process.cwd();
-const PACKAGE_FOLDER = path.resolve(__dirname, "../");
-const TEMPLATES_FOLDER = path.join(PROJECT_FOLDER, "templates");
-
-const DESTINATION_FOLDER = path.join(PROJECT_FOLDER, "tmp");
-console.log({ PROJECT_FOLDER, PACKAGE_FOLDER });
-
-const isObject = (value) => {
-	return value !== null && typeof value === "object";
-};
-
-const merge = (target, ...sources) => {
-	if (sources.length === 0) {
-		return target;
-	}
-
-	const source = sources[0];
-
-	for (const key in source) {
-		const sourceValue = source[key];
-		const targetValue = target[key];
-
-		if (isObject(sourceValue) && isObject(targetValue)) {
-			target[key] = merge(targetValue, sourceValue);
-		} else {
-			target[key] = sourceValue;
-		}
-	}
-
-	sources.shift();
-
-	return merge(target, ...sources);
-};
-
-const exec = (command) => {
-	return childProcess
-		.execSync(command)
-		.toString()
-		.replace(/(\r?\n|\r)$/, "");
-};
-
-const writeFileToProject = (baseName, content, checkIfExists) => {
-	if (checkIfExists) {
-		const destFileName = path.join(PROJECT_FOLDER, baseName);
-		if (fs.existsSync(destFileName)) {
-			throw new Error(
-				`File ${baseName} already exists, please consider reviewing it manually`
-			);
-		}
-	}
-
-	return fsPromises.writeFile(
-		path.join(DESTINATION_FOLDER, baseName),
-		content,
-		"utf-8"
-	);
-};
+const { exec, merge, writeFileToProject } = require("./helpers");
+const { PROJECT_FOLDER, TEMPLATES_FOLDER } = require("./constants");
 
 const processPkg = async () => {
 	const repositoryUrl = exec("git remote get-url --push origin");
@@ -97,25 +40,35 @@ const processPkg = async () => {
 		pkgConfig = merge(require(targetPkg), pkgConfig);
 	}
 
-	await writeFileToProject("package.json", JSON.stringify(pkgConfig));
+	await writeFileToProject(
+		"package.json",
+		JSON.stringify(pkgConfig, null, 2)
+	);
+};
+
+const processTemplates = async () => {
+	const files = await fsPromises.readdir(TEMPLATES_FOLDER);
+
+	return Promise.all(
+		files.map(async (file) => {
+			const templateContent = await fsPromises.readFile(
+				path.join(TEMPLATES_FOLDER, file),
+				"utf-8"
+			);
+
+			await writeFileToProject(file, templateContent, true);
+		})
+	);
 };
 
 async function run() {
 	// @todo: future version => add possibility to specifiy language source (typescript / javascript / ...) via cli args ?
-	const files = await fsPromises.readdir(TEMPLATES_FOLDER);
 
-	files.forEach(async (file) => {
-		const templateContent = await fsPromises.readFile(
-			path.join(TEMPLATES_FOLDER, file),
-			"utf-8"
-		);
-		// @todo: cli feedback
-		await writeFileToProject(file, templateContent, true);
-	});
+	// @todo: cli feedback
+	// @todo: ts-config
 
+	await processTemplates();
 	await processPkg();
-
-	// @todo: npm i
 }
 
 run();
