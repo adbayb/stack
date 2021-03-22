00c4ead3a3ec4d231ba0d156f6dc1a547e377af0
Ayoub Adib <adbayb@gmail.com>
2021-03-22 23:46:23 +0100

    feat(eslint-config): update rules

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,63 +1,193 @@
 module.exports = {
-	extends: [
-		"eslint:recommended",
-		"plugin:prettier/recommended",
-		"plugin:react/recommended",
-		"plugin:react-hooks/recommended",
-	],
-	plugins: ["import"],
+	root: true,
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		jest: true,
+		node: true,
+	},
 	parserOptions: {
-		ecmaVersion: 2020,
+		ecmaVersion: 2018,
 		sourceType: "module",
 		ecmaFeatures: {
 			jsx: true,
 		},
 	},
-	env: {
-		browser: true,
-		commonjs: true,
-		es2020: true,
-		jest: true,
-		node: true,
-		serviceworker: true,
-		worker: true,
-	},
 	settings: {
+		"import/resolver": {
+			node: {
+				extensions: [".js", ".jsx", ".ts", ".tsx"],
+			},
+		},
 		react: {
 			version: "detect",
 		},
 	},
+	extends: ["eslint:recommended", "plugin:prettier/recommended"],
+	plugins: ["jest", "import", "react", "react-hooks", "sonarjs"],
 	overrides: [
 		{
-			files: ["**/*.{ts,tsx}"],
+			files: ["**/*.ts?(x)"],
 			parser: "@typescript-eslint/parser",
 			extends: [
-				"plugin:@typescript-eslint/recommended",
-				"prettier/@typescript-eslint",
+				// @note: following line disables recommended eslint rules already checked by TS:
+				"plugin:@typescript-eslint/eslint-recommended",
 			],
-			parserOptions: {
-				warnOnUnsupportedTypeScriptVersion: true,
-			},
+			plugins: ["@typescript-eslint"],
 			rules: {
-				"@typescript-eslint/ban-ts-comment": "off",
-				"@typescript-eslint/explicit-module-boundary-types": "off",
-				"@typescript-eslint/no-empty-interface": "off",
-				"@typescript-eslint/no-non-null-assertion": "off",
-				// @note: for preact h primitive
+				"@typescript-eslint/adjacent-overload-signatures": "error",
+				// @todo: change to error once all warnings are fixed
+				"@typescript-eslint/ban-ts-comment": "warn",
+				// @todo: change to error once all warnings are fixed
+				"@typescript-eslint/ban-types": "warn",
+				"no-array-constructor": "off",
+				"@typescript-eslint/no-array-constructor": "error",
+				"@typescript-eslint/no-empty-interface": "error",
+				// @todo: change to error once all warnings are fixed
+				"@typescript-eslint/no-explicit-any": "warn",
+				"@typescript-eslint/no-extra-non-null-assertion": "error",
+				"no-extra-semi": "off",
+				"@typescript-eslint/no-extra-semi": "error",
+				"@typescript-eslint/no-inferrable-types": "error",
+				"@typescript-eslint/no-misused-new": "error",
+				"@typescript-eslint/no-namespace": "error",
+				"@typescript-eslint/no-non-null-asserted-optional-chain": "error",
+				"@typescript-eslint/no-non-null-assertion": "warn",
+				"@typescript-eslint/no-this-alias": "error",
+				"no-unused-vars": "off",
 				"@typescript-eslint/no-unused-vars": [
 					"error",
-					{ args: "none", varsIgnorePattern: "^h$" },
+					{ ignoreRestSiblings: true },
 				],
+				"@typescript-eslint/no-var-requires": "error",
+				"@typescript-eslint/prefer-as-const": "error",
+				"@typescript-eslint/prefer-namespace-keyword": "error",
+				"@typescript-eslint/triple-slash-reference": "error",
+			},
+		},
+		{
+			files: ["**/stories.[jt]s?(x)"],
+			rules: {
+				"no-alert": "off",
+				"react/display-name": "off",
 			},
 		},
 	],
+	// @todo: explicitly set rules and do not rely on recommended (less maintainable)
 	rules: {
-		"dot-notation": "error",
+		// #region eslint
+		"no-alert": "error",
+		"no-var": "error",
+		"object-shorthand": ["error", "always"],
+		"padding-line-between-statements": [
+			"error",
+			{
+				"blankLine": "always",
+				"prev": "*",
+				"next": "*"
+			},
+			{
+				"blankLine": "never",
+				"prev": [
+					"const",
+					"let",
+					"var"
+				],
+				"next": [
+					"const",
+					"let",
+					"var"
+				]
+			},
+			{
+				"blankLine": "never",
+				"prev": [
+					"case",
+					"default"
+				],
+				"next": [
+					"case",
+					"default"
+				]
+			},
+			{
+				"blankLine": "any",
+				"prev": [
+					"expression"
+				],
+				"next": [
+					"expression"
+				]
+			},
+			{
+				"blankLine": "any",
+				"prev": [
+					"multiline-const",
+					"multiline-let",
+					"multiline-var"
+				],
+				"next": [
+					"const",
+					"let",
+					"var"
+				]
+			},
+			{
+				"blankLine": "any",
+				"prev": [
+					"const",
+					"let",
+					"var"
+				],
+				"next": [
+					"multiline-const",
+					"multiline-let",
+					"multiline-var"
+				]
+			},
+			{
+				"blankLine": "any",
+				"prev": [
+					"cjs-import",
+					"import"
+				],
+				"next": [
+					"cjs-import",
+					"import"
+				]
+			}
+		],
+		"prefer-const": "error",
+		"sort-imports": ["error", { ignoreDeclarationSort: true }],
+		// #endregion
+		// #region import
+		"import/export": "error",
+		"import/default": "error",
+		"import/first": "error",
+		"import/named": "error",
+		"import/namespace": "error",
+		"import/newline-after-import": "error",
+		"import/no-absolute-path": "error",
+		"import/no-amd": "error",
+		"import/no-cycle": "error",
 		"import/no-default-export": "error",
+		"import/no-duplicates": "error",
+		"import/no-mutable-exports": "error",
+		"import/no-named-as-default": "error",
+		"import/no-named-as-default-member": "error",
+		"import/no-self-import": "error",
+		"import/no-useless-path-segments": [
+			"error",
+			{
+				noUselessIndex: true,
+				commonjs: true,
+			},
+		],
 		"import/order": [
 			"error",
 			{
-				"newlines-between": "never",
+				"newlines-between": "always",
 				groups: [
 					["builtin", "external"],
 					"internal",
@@ -67,105 +197,92 @@ module.exports = {
 				],
 			},
 		],
-		"lines-between-class-members": [
-			"error",
-			"always",
-			{ exceptAfterSingleLine: true },
-		],
-		"no-underscore-dangle": "error",
-		"no-unused-vars": [
-			"error",
+		// #endregion
+		// #region jest
+		"jest/consistent-test-it": ["error", { fn: "test" }],
+		"jest/expect-expect": "error",
+		"jest/prefer-todo": "error",
+		"jest/no-commented-out-tests": "error",
+		"jest/no-conditional-expect": "error",
+		"jest/no-deprecated-functions": "error",
+		"jest/no-disabled-tests": "error",
+		"jest/no-done-callback": "error",
+		"jest/no-export": "error",
+		"jest/no-focused-tests": "error",
+		"jest/no-identical-title": "error",
+		"jest/no-jasmine-globals": "error",
+		"jest/no-jest-import": "error",
+		"jest/no-mocks-import": "error",
+		"jest/no-standalone-expect": "error",
+		"jest/no-test-prefixes": "error",
+		"jest/prefer-hooks-on-top": "error",
+		"jest/prefer-spy-on": "error",
+		// @todo: change to error once all warnings are fixed
+		"jest/prefer-strict-equal": "warn",
+		"jest/prefer-to-be-null": "error",
+		"jest/prefer-to-be-undefined": "error",
+		"jest/prefer-to-contain": "error",
+		"jest/prefer-to-have-length": "error",
+		// @todo: change to error once all warnings are fixed
+		"jest/valid-title": [
+			"warn",
 			{
-				ignoreRestSiblings: true,
+				mustMatch: { test: "should" },
 			},
 		],
-		"padding-line-between-statements": [
-			"error",
-			{
-				blankLine: "always",
-				prev: [
-					"block",
-					"block-like",
-					"class",
-					"directive",
-					"expression",
-					"const",
-					"let",
-					"var",
-					"import",
-					"cjs-import",
-					"export",
-					"cjs-export",
-				],
-				next: "*",
-			},
-			{
-				blankLine: "always",
-				prev: "*",
-				next: "return",
-			},
-			{
-				blankLine: "never",
-				prev: ["expression"],
-				next: ["expression"],
-			},
-			{
-				blankLine: "never",
-				prev: ["const", "let", "var"],
-				next: ["const", "let", "var"],
-			},
-			{
-				blankLine: "always",
-				prev: ["cjs-import"],
-				next: ["*"],
-			},
-			{
-				blankLine: "never",
-				prev: ["case"],
-				next: ["case", "default"],
-			},
-			{
-				blankLine: "any",
-				prev: ["multiline-const", "multiline-let", "multiline-var"],
-				next: [
-					"multiline-const",
-					"multiline-let",
-					"multiline-var",
-					"const",
-					"let",
-					"var",
-				],
-			},
-			{
-				blankLine: "any",
-				prev: ["export"],
-				next: ["export"],
-			},
-			{
-				blankLine: "any",
-				prev: ["cjs-export"],
-				next: ["cjs-export"],
-			},
-			{
-				blankLine: "any",
-				prev: ["import"],
-				next: ["import", "cjs-import"],
-			},
-			{
-				blankLine: "any",
-				prev: ["cjs-import"],
-				next: ["cjs-import", "import"],
-			},
-		],
-		"prefer-destructuring": [
-			"error",
-			{
-				AssignmentExpression: {
-					array: true,
-					object: false,
-				},
-			},
-		],
-		"sort-imports": ["error", { ignoreDeclarationSort: true }],
+		// #endregion
+		// #region react
+		"react/display-name": "error",
+		"react/jsx-boolean-value": "error",
+		"react/jsx-fragments": "error",
+		"react/jsx-key": "error",
+		"react/jsx-no-useless-fragment": "warn",
+		"react/jsx-pascal-case": "error",
+		"react/jsx-uses-react": "error",
+		"react/jsx-uses-vars": "error",
+		"react/no-children-prop": "error",
+		"react/no-danger": "error",
+		"react/no-deprecated": "error",
+		"react/no-direct-mutation-state": "error",
+		"react/no-is-mounted": "error",
+		"react/no-render-return-value": "error",
+		"react/no-string-refs": "error",
+		"react/no-unescaped-entities": "error",
+		"react/prefer-stateless-function": "error",
+		// #endregion react
+		// #region react-hooks
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+		// #endregion react-hooks
+		// #region sonarjs
+		// @todo: change to error once all warnings are fixed
+		"sonarjs/cognitive-complexity": "warn",
+		"sonarjs/max-switch-cases": "error",
+		"sonarjs/no-all-duplicated-branches": "error",
+		"sonarjs/no-collapsible-if": "error",
+		"sonarjs/no-collection-size-mischeck": "error",
+		// @todo: change to error once all warnings are fixed
+		"sonarjs/no-duplicate-string": "warn",
+		"sonarjs/no-duplicated-branches": "error",
+		"sonarjs/no-element-overwrite": "error",
+		"sonarjs/no-extra-arguments": "error",
+		"sonarjs/no-identical-conditions": "error",
+		// @todo: change to error once all warnings are fixed
+		"sonarjs/no-identical-functions": "warn",
+		"sonarjs/no-identical-expressions": "error",
+		"sonarjs/no-inverted-boolean-check": "error",
+		"sonarjs/no-one-iteration-loop": "error",
+		"sonarjs/no-redundant-boolean": "error",
+		"sonarjs/no-redundant-jump": "error",
+		"sonarjs/no-same-line-conditional": "error",
+		"sonarjs/no-small-switch": "error",
+		"sonarjs/no-unused-collection": "error",
+		"sonarjs/no-use-of-empty-return-value": "error",
+		"sonarjs/no-useless-catch": "error",
+		"sonarjs/prefer-immediate-return": "error",
+		"sonarjs/prefer-object-literal": "error",
+		"sonarjs/prefer-single-boolean-return": "error",
+		"sonarjs/prefer-while": "error",
+		// #endregion
 	},
 };
