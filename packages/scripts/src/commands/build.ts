5f0ebc5deaa482cf6d3fac3cb611c593e8293e30
Ayoub Adib <adbayb@gmail.com>
2021-04-02 01:13:27 +0200

    feat(scripts): read target from tsconfig and presets from pkg metadata

--- packages/scripts/src/commands/build.ts
+++ packages/scripts/src/commands/build.ts
@@ -1,46 +1,95 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { BuildOptions, build } from "esbuild";
 import { resolve } from "path";
+import { BuildOptions, build } from "esbuild";
+import ts from "typescript";
 import { CWD } from "../constants";
 import { run } from "../helpers";
 
-// @todo: invariant checks (if no source field is provided in package.json => error)
+// @todo: invariant/assert checks (if no source field is provided in package.json => error)
 // @todo: clean before building (share clean method with the clean command)
-// @todo: esm, cjs, umd builds depending on package.json fields
-
-const pkgMetadata = require(resolve(CWD, "package.json"));
-const tsMetadata = require(resolve(CWD, "tsconfig.json"));
-
+// @todo: support externals
 // @todo: run tsc to emit declaration file based upon pkgMetadata target
-console.warn(tsMetadata, __dirname);
 
 type BundleFormat = NonNullable<BuildOptions["format"]>;
+type PackageMetadata = {
+	main: string;
+	module: string;
+	source: string;
+	dependencies?: Record<string, string>;
+	devDependencies?: Record<string, string>;
+	peerDependencies?: Record<string, string>;
+};
 
-const bundle = (format: BundleFormat, isProduction?: boolean) => {
-	const outfile = format === "esm" ? pkgMetadata.module : pkgMetadata.main;
+const isObject = (value: unknown): value is Record<string, string> => {
+	return value !== null && typeof value === "object";
+};
 
-	return build({
-		absWorkingDir: CWD,
-		bundle: true,
-		define: {
-			"process.env.NODE_ENV": isProduction
-				? '"production"'
-				: '"development"',
-		},
-		entryPoints: [pkgMetadata.source],
-		outfile,
-		tsconfig: "tsconfig.json",
-		target: "es2015",
-		format,
-		minify: isProduction,
-		sourcemap: !isProduction,
-		// @todo: inject only if react-jsx / react-jsx-dev is defined (use typescript api to read the whole flatten tsconfig)
-		// @todo: choose preact or react preset based upon pkg dependencies! (if dependencies.preact key exists -> preact preset -> else if dependencies.react key exists -> react presets -> else do nothing)
-		inject: [resolve(__dirname, "../../public/buildPresets/preact.js")],
-	});
+const getInjectPresets = (pkg: PackageMetadata): BuildOptions["inject"] => {
+	const dependencyField = pkg.dependencies;
+
+	if (!isObject(dependencyField)) {
+		return;
+	}
+
+	const dependencies = Object.keys(dependencyField);
+	const availablePresets = ["preact", "react"]; // @note: the order is important (search first preact before react)
+	const getPreset = (name: string) => {
+		return resolve(__dirname, `../../public/buildPresets/${name}.js`);
+	};
+
+	for (const preset of availablePresets) {
+		if (dependencies.includes(preset)) {
+			return [getPreset(preset)];
+		}
+	}
+
+	return;
+};
+
+const createBundler = () => {
+	// @note: always get fresh package.json data while running bundle command:
+	const pkgMetadata: PackageMetadata = require(resolve(CWD, "package.json"));
+	const tsMetadata = ts.parseJsonConfigFileContent(
+		require(resolve(CWD, "tsconfig.json")),
+		ts.sys,
+		CWD
+	).options;
+
+	// @todo: prevent issues if no typescript or tsconfig provided
+	const tsTarget = tsMetadata.target || ts.ScriptTarget.ESNext;
+	// @note: convert ts target value to esbuild ones (latest value is not supported)
+	const target = [ts.ScriptTarget.ESNext, ts.ScriptTarget.Latest].includes(
+		tsTarget
+	)
+		? "esnext"
+		: ts.ScriptTarget[tsTarget]?.toLowerCase();
+
+	return (format: BundleFormat, isProduction?: boolean) => {
+		const outfile =
+			format === "esm" ? pkgMetadata.module : pkgMetadata.main;
+
+		return build({
+			absWorkingDir: CWD,
+			bundle: true,
+			define: {
+				"process.env.NODE_ENV": isProduction
+					? '"production"'
+					: '"development"',
+			},
+			entryPoints: [pkgMetadata.source],
+			outfile,
+			tsconfig: "tsconfig.json",
+			target,
+			format,
+			minify: isProduction,
+			sourcemap: !isProduction,
+			inject: getInjectPresets(pkgMetadata),
+		});
+	};
 };
 
 const main = async () => {
+	const bundle = createBundler();
 	const formats: BundleFormat[] = ["cjs", "esm"];
 
 	for (const format of formats) {
