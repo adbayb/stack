433e86fc92bfc3498598e480362ef301dcd945a4
Ayoub Adib <adbayb@gmail.com>
2022-03-26 01:29:47 +0100

    feat(prettier-config): add singleAttributePerLine property

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -6,6 +6,10 @@ setup();
 
 const command = process.argv[2] || "";
 
+// @todo: set postinstall script on package.json to set pre-commit and commit hooks
+// @todo: add eslint, prettier, lint-staged, commitlint as dependencies (not exposed externally)
+// @todo: add typescript as peerDependencies optional since tsconfig is anyway exposed externally (for ide intellisense)
+
 import(`./commands/${command}`)
 	.then((module) => module.main())
 	.catch((error) => {
