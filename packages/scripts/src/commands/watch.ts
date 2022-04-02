781ae62925cf4900d4d97e58749585e229e4e9a0
Ayoub Adib <adbayb@gmail.com>
2022-04-02 16:32:28 +0200

    feat(scripts): do not clean assets on watch command

--- packages/scripts/src/commands/watch.ts
+++ packages/scripts/src/commands/watch.ts
@@ -1,16 +1,11 @@
 import { CommandFactory } from "@adbayb/scripts/src/types";
-import { execQuickbundle, execScripts } from "../helpers";
+import { execQuickbundle } from "../helpers";
 
 export const createWatchCommand: CommandFactory = (program) => {
 	program
 		.command({
 			name: "watch",
 			description: "Watch and rebuild assets on any code change",
-		})
-		.task({
-			handler() {
-				return execScripts("clean");
-			},
 		})
 		.task({
 			handler() {
