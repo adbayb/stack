629fda5fcdae07ba27a9fc7d60dcbc034f7525f1
Ayoub Adib <adbayb@gmail.com>
2023-08-09 10:03:08 +0200

    chore: add config type whenever possible

--- packages/prettier-config/index.cjs
+++ packages/prettier-config/index.cjs
@@ -1,19 +1,23 @@
+/**
+ * @type {import("prettier").Config}
+ */
 module.exports = {
-	"$schema": "http://json.schemastore.org/prettierrc",
-	"arrowParens": "always",
-	"bracketSpacing": true,
-	"endOfLine": "lf",
-	"printWidth": 80,
-	"semi": true,
-	"singleAttributePerLine": true,
-	"singleQuote": false,
-	"trailingComma": "all",
-	"useTabs": true,
-    plugins: [require.resolve('prettier-plugin-astro')],
-    overrides: [{
-        files: '*.astro',
-        options: {
-            parser: 'astro',
-        },
-    }],
+	arrowParens: "always",
+	bracketSpacing: true,
+	endOfLine: "lf",
+	printWidth: 80,
+	semi: true,
+	singleAttributePerLine: true,
+	singleQuote: false,
+	trailingComma: "all",
+	useTabs: true,
+	plugins: [require.resolve("prettier-plugin-astro")],
+	overrides: [
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
 };
