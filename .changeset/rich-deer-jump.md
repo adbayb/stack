edd902128fdc4dfaa0358a733347a2671f2b73b7
Ayoub Adib <adbayb@gmail.com>
2026-08-09 13:42:59 +0200

    fix: approve esbuild builds to prevent creation errors

---
+++ .changeset/rich-deer-jump.md
@@ -0,0 +1,5 @@
+---
+"@adbayb/stack": patch
+---
+
+Approve `esbuild` builds (used in vitest and tsx) to prevent project creation errors.
--- stack/templates/multi-projects/libraries/{{projectName}}/src/index.test.ts
+++ stack/templates/multi-projects/libraries/{{projectName}}/src/index.test.ts
@@ -1,5 +1,7 @@
 import { describe, test } from "vitest";
 
+// oxlint-disable-next-line vitest/warn-todo
 describe.todo("xxx", () => {
+	// oxlint-disable-next-line vitest/warn-todo
 	test.todo("should xxx");
 });
